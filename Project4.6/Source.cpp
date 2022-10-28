74f395f461f150d1969d2ca727c7ee31df9b9e1b
Anastasiaza333 <anastasiaza05@gmail.com>
2022-10-28 12:21:21 +0300

    Project4.6

---
+++ Project4.6/Source.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double S, S1;
+	int n, k;
+	S= 0 ;
+	k = 1;
+	while (k <= 10)
+
+	{
+		S1=0;
+		n = 1;
+		while (n <= k)
+
+		{
+			S1+= sin(1. * k*n);
+			n++;
+
+		}
+		S +=S1/ k;
+		k++;
+
+	}
+	cout << S << endl;
+	S = 0;
+	k = 1;
+	n = 1;
+	do
+	{
+		S1= 0;
+		n = 1;
+		do
+		{
+			S1+= sin(1. * k * n);
+			n++;
+		} while (n <= k);
+		S += S1 / k;
+		k++;
+	} while (k <= 10);
+	cout << S << endl;
+	S=0;
+	for (k = 1; k <= 10; k++)
+
+	{
+		S1= 0;
+		for (n= 1; n<= k;n++)
+
+		{
+			S1+= sin(1. * k * n);
+
+		}
+		S +=S1/ k;
+
+	}
+	cout << S << endl;
+	S = 0;
+	for (k= 10; k>= 1; k--)
+
+	{
+		S1= 0;
+		for (n=k; n>= 1; n--)
+
+		{
+			S1+= sin(1. * k * n);
+			
+		}
+		S += S1 / k;
+	}
+	cout << S<< endl;
+	return 0;
+}
